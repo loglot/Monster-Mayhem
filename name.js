f5cc1af39a47628c952652702b4cd2fe02c8fdb3
lynkslol <rockfellor1273@gmail.com>
2023-12-13 11:07:37 -0600

    drydrydry

--- name.js
+++ name.js
@@ -1,2 +1,60 @@
 const canvas = document.getElementById("game_screen");
 const ctx = canvas.getContext("2d");
+
+var x = 50
+var y = 50
+var velX = 0
+var velY = 0
+
+Input()
+while(true) {
+    x+= velX 
+    y+= velY
+    velX = velX *.95
+    velY = velY *.99
+    draw()
+    await sleep(1000/60)
+}
+
+function draw() {
+    ctx.fillStyle = "#ADD8E6";
+    ctx.rect(0, 0, 100000, 10000)
+    ctx.fill()
+
+    ctx.fillStyle = "#000";
+
+
+    ctx.beginPath();
+    ctx.arc(x, y, 100, 100, 1.8 * Math.PI);
+    ctx.fill();
+    ctx.closePath();
+}
+
+async function sleep(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+function Input() { 
+    document.addEventListener('keydown', (event) => {
+    var name = event.key;
+    var code = event.code;
+    //alert(`Key pressed ${name} \r\n Key code value: ${code}`);
+    if (code == "ArrowRight") {
+        velX+= 5
+    }
+    if (code == "ArrowDown") {
+        velY+= 5
+    }
+    if (code == "ArrowUp") {
+        velY-= 5
+    }
+    if (code == "ArrowLeft") {
+        velX-= 5
+    }
+    if (code == "Space") {
+        velX *= 2
+        velY *= 2
+    }
+
+  }, false);
+}

00075110b90615a1a64d1443ade5a4feaea7682b
lynks <94482520+lynkslol@users.noreply.github.com>
2023-12-14 15:12:59 +0000

    grid + camera update + player outline

--- name.js
+++ name.js
@@ -5,6 +5,11 @@ var x = 50
 var y = 50
 var velX = 0
 var velY = 0
+var camX = 0
+var camY = 0
+var anything = new Image()
+anything.src = 'https://fxfactory.com/downloads/docs/noiseindustries/fxfactorypro/Thumbnails/Checkerboard.jpg';
+
 
 Input()
 while(true) {
@@ -12,20 +17,31 @@ while(true) {
     y+= velY
     velX = velX *.95
     velY = velY *.99
+    camX = x
+    camY = y
+
     draw()
     await sleep(1000/60)
 }
 
 function draw() {
-    ctx.fillStyle = "#ADD8E6";
-    ctx.rect(0, 0, 100000, 10000)
+    ctx.beginPath()
+    ctx.fillStyle = "#85c963";
+    ctx.rect(0, 0, 100000, 100000)
     ctx.fill()
+    ctx.closePath();
+
+    ctx.drawImage(anything, 580-camX, 650-camY, 5000, 5000)
 
     ctx.fillStyle = "#000";
-
-
     ctx.beginPath();
-    ctx.arc(x, y, 100, 100, 1.8 * Math.PI);
+    ctx.arc(838, 459, 125, 100, 1.8 * Math.PI)
+    ctx.fill();
+    ctx.closePath();
+
+    ctx.fillStyle = "#AEC6CF";
+    ctx.beginPath();
+    ctx.arc(838, 459, 100, 100, 1.8 * Math.PI)
     ctx.fill();
     ctx.closePath();
 }
@@ -39,19 +55,21 @@ function Input() {
     var name = event.key;
     var code = event.code;
     //alert(`Key pressed ${name} \r\n Key code value: ${code}`);
-    if (code == "ArrowRight") {
+    if (code == "ArrowRight" || code == "KeyD") {
         velX+= 5
     }
-    if (code == "ArrowDown") {
+    if (code == "ArrowDown" || code == "KeyS") {
         velY+= 5
     }
-    if (code == "ArrowUp") {
+    if (code == "ArrowUp" || code == "KeyW") {
         velY-= 5
     }
-    if (code == "ArrowLeft") {
+    if (code == "ArrowLeft" || code == "KeyA") {
         velX-= 5
+        
     }
-    if (code == "Space") {
+    if (code == "ShiftLeft") {
+        //x = Math.max(0, x - 1);
         velX *= 2
         velY *= 2
     }
